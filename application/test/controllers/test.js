676e83a4b0922c93b5fa5434c32f8237f07071b1
kinny office <vuong@yahoo.com>
2018-01-22 10:42:52 +0700

    search

--- application/test/controllers/test.js
+++ application/test/controllers/test.js
@@ -6,7 +6,28 @@ var autoIncrement = require("mongodb-autoincrement"); // auto inc
 var d = +new Date();
 
 router.get('/', function(req, res){
-  res.render('test/views/download');
+	res.render('test/views/download');
 })
+
+router.get('/search', function(req, res){
+	var url='https://api.4shared.com/v1_2/files.json?oauth_consumer_key=44f6ae5d9c0e192866d3412db168dfe8&category=3&offset=20&limit=10';
+	request({
+		url: url,
+		json: true
+	}, function (error, response, body) {
+		if(error) throw error;
+
+		
+        console.log(body) // Print the json response
+        res.json(body);
+
+    
+})
+})
+
+// router.get('/download', function(req, res, next){
+// 	https://api.4shared.com/v1_2/files/89BHBylb/download
+// })
+
 
 module.exports = router;
